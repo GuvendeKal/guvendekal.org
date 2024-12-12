f2643ca9dde77da836499c2a775c67f394555dbc
Wise <wisewebworks@outlook.com>
2024-12-12 19:41:34 +0000

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Burada özel kurallar ekleyebilirsiniz
+  },
+};
